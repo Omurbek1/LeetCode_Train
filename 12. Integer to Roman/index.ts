2f521ef1c5b894656b8ddeacefe1d8c3c131935d
Omurbek Mamytbekov <omurbekmamytbekov545@gmail.com>
2022-10-14 15:59:22 +0600

    '13integertoroman'

---
+++ 12. Integer to Roman/index.ts
@@ -0,0 +1,12 @@
+let intToRoman = function(num) {
+    const M = {
+        'M': 1000, 'CM': 900, 'D': 500, 'CD': 400, 'C': 100, 'XC': 90, 'L': 50, 'XL': 40, 'X': 10, 'IX': 9,
+        'V': 5, 'IV': 4, 'I': 1
+    }
+    let result = ''
+    Object.entries(M).forEach(([k, v]) => {
+        result += k.repeat(Math.floor(num / v))
+        num%=v
+    })
+    return result
+}
