da47bf3383def84dbf2e7492f6ee7a43b4a2776d
Omurbek Mamytbekov <omurbekmamytbekov545@gmail.com>
2022-10-17 10:03:21 +0600

    List'

--- 19. Remove Nth Node From End of List/index.jsx
+++ 19. Remove Nth Node From End of List/index.jsx
@@ -1,3 +1,6 @@
+
+// 19. Remove Nth Node From End of List
+
 const removeNthFromEnd = function (head, n) {
     let dummyHead = new ListNode(-Infinity)
     dummyHead.next = head
@@ -9,7 +12,6 @@ const removeNthFromEnd = function (head, n) {
         counnt ++
         tail = tail.next
     }
-
     let removedNode = head
     let prev = dummyHead;
 
