b64ac3b40558389875f2077ebeca4d5d42055c46
Omurbek Mamytbekov <omurbekmamytbekov545@gmail.com>
2022-10-18 09:10:40 +0600

    33. Search in Rotated Sorted Array

--- 33. Search in Rotated Sorted Array/index.ts
+++ 33. Search in Rotated Sorted Array/index.ts
@@ -1,3 +1,31 @@
 const search = function(nums, target) {
+    let start = 0 , end = nums.length - 1;
     
+
+    while (start <= end) {
+        const mid = Math.floor((start + end) / 2);
+        if(nums[mid]===target) return mid
+       
+        if (nums[mid] >= nums[start]) {
+            if (target >= nums[start] && target < nums[mid]) {
+                end=mid-1
+            } else {
+                start=mid+1
+            }
+
+        } else {
+            if (target > nums[mid] && target <= nums[end]) {
+                start=mid+1
+            } else {
+                end=mid-1
+            }
+        }
+        // if (nums[mid] === target) return mid
+        // if (nums[mid] > target) {
+        //     start = mid + 1;
+        // } else {
+        //     end=mid-1
+        // }
+    }
+    return -1
 };
