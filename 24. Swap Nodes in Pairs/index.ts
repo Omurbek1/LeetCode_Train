ba60404bcd9251c3d28ae2305e7d89d4d5d937c1
Omurbek Mamytbekov <omurbekmamytbekov545@gmail.com>
2022-10-20 17:55:13 +0600

    24. Swap Nodes in Pairs

--- 24. Swap Nodes in Pairs/index.ts
+++ 24. Swap Nodes in Pairs/index.ts
@@ -10,5 +10,16 @@
  * @return {ListNode}
  */
  var swapPairs = function(head) {
-    const dummy
+     const dummy = new ListNode();
+     let temp=dummy
+     dummy.next = head;
+     while (temp.next && temp.next.next) {
+         let node1 = temp.next;
+         let node2 = temp.next.next
+         temp.next = node2
+         node1.next = node2.next;
+         node2.next=node1
+         temp=node1
+     }
+     return dummy.next
 };
