c8ba4dc7e171153abcd750cc69de76a715b8f8c5
Omurbek Mamytbekov <omurbekmamytbekov545@gmail.com>
2022-10-21 10:06:42 +0600

    29 Divide Two Integers

--- 29. Divide Two Integers/indext.ts
+++ 29. Divide Two Integers/indext.ts
@@ -1,3 +1,17 @@
 var divide = function(dividend, divisor) {
+    if (dividend == 0) {
+        return 0
+    } else if (divisor === 1) {
+        return dividend
+    } else if (divisor == -1) {
+        if (dividend == -2147483648) {
+            return 2147483648
+        }
+        return -dividend;
+    }
+    var minus = (dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0) ? true : false
     
+    let dd = Math.abs(dividend)
+    let dv = Math.abs(divisor)
+    let res =0,i=1 , tmp=dv
 };
