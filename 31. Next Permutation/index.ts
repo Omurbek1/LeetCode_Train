c81ca90f60a75200edac1e8f700df1500fc36730
Omurbek Mamytbekov <omurbekmamytbekov545@gmail.com>
2022-10-22 10:54:15 +0600

    31. Next Permutation

--- 31. Next Permutation/index.ts
+++ 31. Next Permutation/index.ts
@@ -2,17 +2,24 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
- var nextPermutation = function(nums) {
-     let i = nums.length - 2;
-     while (nums[i] >= nums[i + 1]) {
-         i--
-        
-     }
-     if (i === -1) {
-         nums.sort((a, b) => a - b)
-         
-     } else {
-         nums[i]
-     }
-     
+var nextPermutation = function(nums) {
+	let i = nums.length - 2;
+	while (nums[i] >= nums[i + 1]) {
+		i--;
+	}
+	if (i === -1) {
+		nums.sort((a, b) => a - b);
+    } else {
+        let j=nums.length-1
+        while (nums[i] >= nums[j]) {
+           j--
+        }
+        [nums[i], nums[j] = nums[j], nums[i]]
+        let left = i + 1, right = nums.length - 1
+        while (left<right) {
+            [nums[left], nums[right]] = [nums[right], nums[left]]
+            left++;
+            right--
+        }
+	}
 };
