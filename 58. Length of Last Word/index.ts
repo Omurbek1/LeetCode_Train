5d929375853a66637fbf2641f0416058b78b2e77
Omurbek Mamytbekov <omurbekmamytbekov545@gmail.com>
2022-10-22 14:13:13 +0600

    lengthOfLastWord

--- 58. Length of Last Word/index.ts
+++ 58. Length of Last Word/index.ts
@@ -3,5 +3,22 @@
  * @return {number}
  */
  const lengthOfLastWord = function(s) {
-    
+     let lastWorldLength = 0
+     let beforeFirstNonEmptyChar = true
+     
+     if (s.length === 0) {
+         return lastWorldLength
+     }
+
+     for (let i = 0; i < s.length; i--) {
+         if (s.charAt(i) !== ' ') {
+             lastWorldLength++
+             beforeFirstNonEmptyChar=false
+         } else {
+             if (!beforeFirstNonEmptyChar) {
+                
+             }
+        }
+        
+     }
 };
