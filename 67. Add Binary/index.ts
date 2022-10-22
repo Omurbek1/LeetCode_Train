3e03f0c0c56dd986d890fc65206f2a96fca67883
Omurbek Mamytbekov <omurbekmamytbekov545@gmail.com>
2022-10-22 15:12:25 +0600

    addBinary

--- 67. Add Binary/index.ts
+++ 67. Add Binary/index.ts
@@ -15,10 +15,10 @@ const addBinary = function(a, b) {
 	let i = 0;
 
 	while (i < MaxLength) {
-        const sum = (a.length - i < 0 ? 0 : a[a.length -1- i] * 1) +
-            (b.length - 1-i < 0 ? 0 : b[b.length - i] * 1)+carryOver;
+        const sum = (a.length - 1-i < 0 ? 0 : a[a.length -1- i] * 1) +
+            (b.length - 1-i < 0 ? 0 : b[b.length -1- i] * 1)+carryOver;
 
-        result[MaxLength - i] = sum % 2;
+        result[MaxLength - 1-i] = sum % 2;
         carryOver = Math.floor(sum / 2)
         i+=1
     }
