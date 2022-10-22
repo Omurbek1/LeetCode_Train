2c649b3078837c8ac5825010f173fcbf8d4210c8
Omurbek Mamytbekov <omurbekmamytbekov545@gmail.com>
2022-10-22 11:03:20 +0600

    31. Next Permutation

--- 31. Next Permutation/index.ts
+++ 31. Next Permutation/index.ts
@@ -7,19 +7,18 @@ var nextPermutation = function(nums) {
 	while (nums[i] >= nums[i + 1]) {
 		i--;
 	}
-	if (i === -1) {
-		nums.sort((a, b) => a - b);
-    } else {
-        let j=nums.length-1
-        while (nums[i] >= nums[j]) {
-           j--
-        }
-        [nums[i], nums[j] = nums[j], nums[i]]
-        let left = i + 1, right = nums.length - 1
-        while (left<right) {
-            [nums[left], nums[right]] = [nums[right], nums[left]]
-            left++;
-            right--
-        }
+	if (i>=0) {
+		let j = nums.length - 1;
+		while (nums[i] >= nums[j]) {
+			j--;
+		}
+        [nums[i], nums[j]] = [nums[j], nums[i]]
+	}
+
+	let left = i + 1, right = nums.length - 1;
+	while (left < right) {
+		[nums[left], nums[right]] = [nums[right], nums[left] ];
+		left++;
+		right--;
 	}
 };
