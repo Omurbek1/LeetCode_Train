8ee7fd5874665f64d98ded625fcb03c9481d097f
Omurbek Mamytbekov <omurbekmamytbekov545@gmail.com>
2022-10-22 10:50:00 +0600

    31. Next Permutation

--- 31. Next Permutation/index.ts
+++ 31. Next Permutation/index.ts
@@ -3,5 +3,10 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
  var nextPermutation = function(nums) {
-    
+     let i = nums.length - 2;
+     while (nums[i] >= nums[i + 1]) {
+         i--
+        
+     }
+     
 };
