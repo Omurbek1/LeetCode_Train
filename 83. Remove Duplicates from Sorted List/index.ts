b888ddbb315e7875914a3fb0621929fac39d4434
Omurbek Mamytbekov <omurbekmamytbekov545@gmail.com>
2022-10-25 09:37:42 +0600

    83. Remove Duplicates from Sorted List

--- 83. Remove Duplicates from Sorted List/index.ts
+++ 83. Remove Duplicates from Sorted List/index.ts
@@ -1,3 +1,22 @@
-var deleteDuplicates = function(head) {
-    
+const deleteDuplicates = function(head) {
+
+    let dummy = new List(-Infinity, head)
+    let curr = head
+    let prev = dummy;
+
+    while (curr) {
+        if (curr.val === prev.val) {
+            while (curr && curr.val===prev.val) {
+                curr = curr.next;
+
+            }
+            prev.next=curr
+             
+        } else {
+            prev.next = curr
+            curr=curr.next
+        }
+        
+    }
+return dummy.next
 };
