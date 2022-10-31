bd9132b073c47333102470eed9a7589d02e850df
Omurbek Mamytbekov <omurbekmamytbekov545@gmail.com>
2022-10-31 16:45:17 +0600

    101 symentric tree

--- 101. Symmetric Tree/index.ts
+++ 101. Symmetric Tree/index.ts
@@ -1,26 +1,37 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {TreeNode} root
- * @return {boolean}
- */
- var isSymmetric = function(root) {
-     let res = true;
-     
-     function helper(node1, node2) {
-         if (!node1 && node2) {
-             return
-         }
-         if (!node1 || node2 || node1.val !== node2.val) {
-             res = false
-             return
-         }
-     }
-     return res;
+var isSymmetric = function (left_tree, right_tree = left_tree) {
+
+    /* -------------------------------------------------------------------------- */
+    /*                             101. Symmetric Tree                            */
+    /* -------------------------------------------------------------------------- */
+
+    /**
+     * @author  Samuel Hinchliffe
+     * @see    {@link linkedin.com/in/samuel-hinchliffe-🚀-2bb5801a5/ | Author's Linkedin }
+     * @see    {@link github.com/Samuel-Hinchliffe}
+    */
+
+     // https://leetcode.com/submissions/detail/684314946/
+
+    // Both are trees are the same
+    if (!left_tree && !right_tree) {
+        return true;
+    }
+
+    // One exists without another?
+    if (!left_tree || !right_tree) {
+        return false;
+    }
+
+    // Are left and right of same value?
+    // If not return false
+    if (left_tree.val != right_tree.val) {
+        return false;
+    }
+
+    // Do all the left trees and right trees
+    let outer_tree = isSymmetric(left_tree.left,  right_tree.right);
+    let inner_tree = isSymmetric(left_tree.right, right_tree.left);
+
+    // Are both trees the same?
+    return outer_tree && inner_tree;
 };
