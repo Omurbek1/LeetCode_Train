67f0c193109e4eb344ab160bd708a08da7f3f10f
Omurbek Mamytbekov <omurbekmamytbekov545@gmail.com>
2022-11-01 18:48:09 +0600

    125. Valid Palindrome

---
+++ 125. Valid Palindrome/index.ts
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+ var isPalindrome = function(s) {
+     const str = s.replace(/\W|_/g, '').toLowerCase();
+     let left = 0, right = str.length - 1;
+     while (left < right) {
+         if (str[left] !== str[right]) {
+             return false
+            
+         }
+         left++
+         right--
+        
+     }
+     return true
+};
