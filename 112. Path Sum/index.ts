b82bade78e4ffec171ee4afc94e9e8a26d29a7da
Omurbek Mamytbekov <omurbekmamytbekov545@gmail.com>
2022-11-04 18:31:54 +0600

    112 Path Sum

--- 112. Path Sum/index.ts
+++ 112. Path Sum/index.ts
@@ -1,16 +1,6 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {TreeNode} root
- * @param {number} targetSum
- * @return {boolean}
- */
- var hasPathSum = function(root, targetSum) {
-    
-};
+var hasPathSum = function(root, sum) {
+    if(!root) return false
+    sum -= root.val
+    if(!root.right && !root.left) return !sum
+    return hasPathSum(root.left, sum) || hasPathSum(root.right, sum)
+}
