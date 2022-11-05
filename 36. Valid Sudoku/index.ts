8920b58dbd3e7fbafc4e6b8ea4122e7acb75b32a
Omurbek Mamytbekov <omurbekmamytbekov545@gmail.com>
2022-11-05 18:34:54 +0600

    36 valid sudoku

--- 36. Valid Sudoku/index.ts
+++ 36. Valid Sudoku/index.ts
@@ -3,23 +3,26 @@
  * @return {boolean}
  */
  var isValidSudoku = function(board) {
-     const row = {}  
-     const box = {}
-     const column = {}
+    let set = new Set()
      
-     for (let i = 0; i < 9; i++){
-         for (let j = 0; j < 9; j++){
-             const value = board[i][j];
-             if (value !== '.') {
-                 const boxidX = Math.floor(i / 3) * 3 + Math.floor(j / 3);
-                 if (row[`${i}-${value}`] || column[`${j}-${value}` || box[`${boxidX}-${value}`]]) {
+     for (let i = 0; i < board.length; i++) {
+         for (let j = 0; j < board[0].length; j++) {
+             const value = board[i][j]
+             if (value !== ".") {
+                 const row = `${value} at row ${i}`
+                 const column = `${value} at column ${j}`
+                 const box = `${value} at box ${Math.floor(i/3)}, ${Math.floor(j/3)}`
+                 console.log(`${row}, ${column}, ${box}`)
+                 if (set.has(row) || set.has(column) || set.has(box)) {
                      return false
+                 } else {
+                     set.add(row)
+                     set.add(column)
+                     set.add(box)
                  }
-                 row[`${i}-${value}`] = true
-                 column[`${j}-${value}`] = true
-                 box[`${boxidX}-${value}`]=true
              }
          }
      }
+ 
      return true
-};
+ };
