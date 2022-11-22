805eae3ee916edbde34d7054015839279e13ede1
Omurbek Mamytbekov <omurbekmamytbekov545@gmail.com>
2022-11-22 16:07:13 +0600

    463 islandPerimeter

--- 463. Island Perimeter/index.ts
+++ 463. Island Perimeter/index.ts
@@ -1,19 +1,19 @@
 function islandPerimeter(grid: number[][]): number {
     var island = 0,
-    nei = 0,
-    m = grid.length,
-    n = grid[0].length;
-    
-for(var i = 0; i < m; i++){
-    for(var j = 0; j < n; j++){
-        if(grid[i][j] == 1){
-            island++; 
-            
-            if(i+1 < m && grid[i+1][j] == 1)nei++;
-            
-            if(j+1 < n && grid[i][j+1] == 1)nei++;
+        nei = 0,
+        m = grid.length,
+        n = grid[0].length;
+
+    for (var i = 0; i < m; i++) {
+        for (var j = 0; j < n; j++) {
+            if (grid[i][j] == 1) {
+                island++;
+
+                if (i + 1 < m && grid[i + 1][j] == 1) nei++;
+
+                if (j + 1 < n && grid[i][j + 1] == 1) nei++;
+            }
         }
     }
-}
-return island * 4 - nei * 2
+    return island * 4 - nei * 2
 };
