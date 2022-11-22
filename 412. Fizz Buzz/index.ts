961ce453dd56e9ac99ac7627abed021cd891c606
Omurbek Mamytbekov <omurbekmamytbekov545@gmail.com>
2022-11-22 10:04:24 +0600

    412. Fizz Buzz

--- 412. Fizz Buzz/index.ts
+++ 412. Fizz Buzz/index.ts
@@ -1,15 +1,16 @@
 function fizzBuzz(n: number): string[] {
-
-    for (let i = 1; i <= n; i++){
-        if (i % 3 && i %5) {
-            console.log('FizBuss')
-            
-        } else if (i % 5) {
-            console.log('BUSS')
+    let result=[]
+    for (let i = 1; i <= n; i++) {
+        if (i%15==0) {
+           result.push('FizzBuzz')
+        } else if (i % 3 === 0) {
+            result.push('Fizz')
         }
-        else {
-            console.log(i);
-            
+        else if (i % 5 === 0) {
+            result.push('Buzz')
+        }else{
+            result.push(i.toString())
         }
     }
+    return (result)
 };
