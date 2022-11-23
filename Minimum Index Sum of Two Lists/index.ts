ad1264e8c587ed80ac5cd44186a7b341f558d2a0
Omurbek Mamytbekov <omurbekmamytbekov545@gmail.com>
2022-11-23 18:12:50 +0600

    Minimum Index Sum of Two Lists js

---
+++ Minimum Index Sum of Two Lists/index.ts
@@ -0,0 +1,21 @@
+function findRestaurant(list1: string[], list2: string[]): string[] {
+    let sum = Infinity
+    const map = list1.reduce((acc, str, index) => {
+      acc[str] = index
+      return acc
+    }, {})
+    for (let i = 0; i < list2.length; i++) {
+      const index1 = map[list2[i]]
+      if (index1 >= 0 && index1 + i < sum) {
+        sum = index1 + i
+      }
+    }
+    const result = []
+    for (let i = 0; i < list2.length; i++) {
+      const index1 = map[list2[i]]
+      if (index1 >= 0 && index1 + i === sum) {
+        result.push(list2[i])
+      }
+    }
+    return result
+};
