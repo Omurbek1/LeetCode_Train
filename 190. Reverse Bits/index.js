8442317ac8e037e24358c550d93b8f8d0e76cc82
Omurbek Mamytbekov <omurbekmamytbekov545@gmail.com>
2022-11-27 11:35:44 +0600

    190. Reverse Bits

---
+++ 190. Reverse Bits/index.js
@@ -0,0 +1,2 @@
+var reverseBits = function (n) { return n.toString(2).padStart(32, '0'); };
+console.log(reverseBits);
---
+++ 190. Reverse Bits/index.ts
@@ -0,0 +1,1 @@
+const reverseBits = (n) =>Number.parseInt( n.toString(2).padStart(32, '0').split('').reverse().join(''),2)
