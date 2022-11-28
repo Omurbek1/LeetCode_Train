5d1bf9b37c6a7246e61c5606117295c7b25b433b
Omurbek Mamytbekov <omurbekmamytbekov545@gmail.com>
2022-11-28 16:46:38 +0600

    111. Minimum Depth of Binary Tree

---
+++ 111. Minimum Depth of Binary Tree/index.ts
@@ -0,0 +1,24 @@
+function minDepth(root: TreeNode | null): number {
+if (root===null) return 0
+
+    let minDepth = 1
+    let queque = [root]
+    
+    while (queque.length) {
+        let levelSize = queque.length
+        while (levelSize) {
+            let current = queque.shift()
+            if (current.left === null && current.right === null) {
+                return minDepth
+
+            } else {
+                if (current.left) queque.push(current.left)
+                if(current.right) queque.push(current.right)
+            }
+            levelSize--
+           
+        }
+        minDepth++
+    }
+    return minDepth
+};
