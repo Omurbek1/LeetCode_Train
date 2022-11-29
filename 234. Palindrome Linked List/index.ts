e2b16cc593a20cfb4cf8d64fafab2a86ceb92e00
Omurbek Mamytbekov <omurbekmamytbekov545@gmail.com>
2022-11-29 21:23:55 +0600

    234. Palindrome Linked List

--- 234. Palindrome Linked List/index.ts
+++ 234. Palindrome Linked List/index.ts
@@ -1,3 +1,30 @@
+/**
+ * 234. Palindrome Linked List
+Easy
+12.5K
+700
+Companies
+Given the head of a singly linked list, return true if it is a 
+palindrome
+ or false otherwise.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,2,1]
+Output: true
+Example 2:
+
+
+Input: head = [1,2]
+Output: false
+ * @param head 
+ * @returns 
+ */
+
+
 const reverse = head => {
     let cur = head;
     let prev = null
@@ -7,6 +34,7 @@ const reverse = head => {
     while (cur) {
         next = cur.next
         cur.next = prev
+        prev=cur
         cur=next
     }
     return prev
