28b5177208345206508a859a6f87355523a6adda
Omurbek Mamytbekov <omurbekmamytbekov545@gmail.com>
2022-12-01 11:59:27 +0600

    205. Isomorphic Strings

--- 205. Isomorphic Strings/index.ts
+++ 205. Isomorphic Strings/index.ts
@@ -1,3 +1,20 @@
 function isIsomorphic(s: string, t: string): boolean {
 
+
+    if (s.length != t.length) {
+        return false
+    }
+    let sArr = new Array(128).fill(0)
+    let tArr = new Array(128).fill(0)
+    
+    for (let i = 0; i < s.length; i++) {
+        let sIdx = s.charCodeAt(i)
+        let tIdx = t.charCodeAt(i)
+        if (sArr[sIdx]!==tArr[tIdx]) {
+            return false
+        }
+        sArr[sIdx] = i + 1
+        tArr[tIdx]=i+1
+    }
+    return true
 };
