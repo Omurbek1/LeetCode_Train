da04e6bcfe6c3848a83e0c954356fbb3a4ff6876
Omurbek Mamytbekov <omurbekmamytbekov545@gmail.com>
2022-12-02 14:54:48 +0600

    1657. Determine if Two Strings Are Close

--- 1657. Determine if Two Strings Are Close/index.java
+++ 1657. Determine if Two Strings Are Close/index.java
@@ -47,17 +47,21 @@ word1 and word2 contain only lowercase English letters.
 
 
 class Solution {
+
     public boolean closeStrings(String word1, String word2) {
         if (word1.length() != word2.length())
             return false;
+
         int[] counts1 = new int[26];
         int[] counts2 = new int[26];
+
         int length = word1.length();
         for (int i = 0; i < length; i++) {
             char c1 = word1.charAt(i), c2 = word2.charAt(i);
             counts1[c1 - 'a']++;
             counts2[c2 - 'a']++;
         }
+        
         for (int i = 0; i < 26; i++) {
             if (counts1[i] == 0 ^ counts2[i] == 0)
                 return false;
