a714535be919f76efd3a5012af2378db96097e9a
Omurbek Mamytbekov <omurbekmamytbekov545@gmail.com>
2022-12-04 16:26:00 +0600

    401. Binary Watch

---
+++ 401. Binary Watch/index.ts
@@ -0,0 +1,13 @@
+var readBinaryWatch = function(num) {
+    const output = [];
+    
+    // find the number of ones in bit expression of time and match with num
+    for (let h = 0; h < 12; h++) {
+        for (let m = 0; m < 60; m++) {
+            const ones = Number(h * 64 + m).toString(2).split('').filter(d => d === '1').length;
+            if (ones === num) output.push(m < 10 ? `${h}:0${m}` : `${h}:${m}`);
+        }
+    }
+    
+    return output;
+};
