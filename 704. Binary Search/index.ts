ef819736aef251193a1665fe0deab2ae07069c1c
Omurbek Mamytbekov <omurbekmamytbekov545@gmail.com>
2022-12-06 20:12:23 +0600

    704. Binary Search

--- 704. Binary Search/index.ts
+++ 704. Binary Search/index.ts
@@ -1,3 +1,16 @@
 function search(nums: number[], target: number): number {
+    let left = 0
+    let right = nums.length - 1
+    while (left <= right) {
+        let mid = left + Math.floor((right - left) / 2)
+        if (nums[mid] === target) {
+            return mid
+        } else if (mid < target) {
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
+    }
+    return -1
+}
 
-};
