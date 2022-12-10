d64848c95654551819d39fb604b154389d12da04
unknown <omurbekmamytbekov545@gmail.com>
2022-12-10 19:20:47 +0600

    1185. Day of the Week

--- 85. Day of the Week/index.ts
+++ 85. Day of the Week/index.ts
@@ -1,3 +1,6 @@
 function dayOfTheWeek(day: number, month: number, year: number): string {
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
+    const dayInt = new Date(year, month - 1, day).getDay()
+    return days[dayInt]
 };
