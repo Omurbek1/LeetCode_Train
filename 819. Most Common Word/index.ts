4658ffb27aab939485276ad4a3b3b119e9a92252
unknown <omurbekmamytbekov545@gmail.com>
2022-12-10 19:16:10 +0600

    1185. Day of the Week

--- 819. Most Common Word/index.ts
+++ 819. Most Common Word/index.ts
@@ -1,3 +1,27 @@
 function mostCommonWord(paragraph: string, banned: string[]): string {
 
+    let bannerSet = new Set(banned)
+    let map = {}
+    let wordArray = paragraph.toLowerCase().match(/[a-z]+/g)
+    for (let i = 0; i < paragraph.length; i++) {
+        if (!bannerSet.has(wordArray[i])) {
+            map[wordArray[i]] = (map[wordArray[i]] | 0) + 1
+
+        }
+
+
+    }
+    let maxCount = 0
+    let maxWord = ""
+
+    for (let key in map) {
+        if (map[key] > maxCount) {
+            maxWord = key;
+            maxCount=map[key]
+        }
+
+    }
+    return maxWord
+
+
 };
