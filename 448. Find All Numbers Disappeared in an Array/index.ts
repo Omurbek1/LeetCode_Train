680c90e4470da6f3500239df4dbc39f53191fb5e
Omurbek <omurbekmamytbekov545@gmail.com>
2023-01-20 19:33:50 +0600

    [200~448. Find All Numbers Disappeared in an Array~

--- 448. Find All Numbers Disappeared in an Array/index.ts
+++ 448. Find All Numbers Disappeared in an Array/index.ts
@@ -1,3 +1,21 @@
+/**
+ * 
+ * @param nums 
+ * @returns 
+ * Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+
+ 
+
+Example 1:
+
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [5,6]
+Example 2:
+
+Input: nums = [1,1]
+Output: [2]
+ */
+
 function findDisappearedNumbers(nums: number[]): number[] {
     const set = new Set(nums) , result = []
     for (let i = 1; i < nums.length+1; i++) { 
