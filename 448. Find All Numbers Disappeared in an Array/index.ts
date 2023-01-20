c1ec3cc402b373365efd2130156a60f5358e4992
Omurbek <omurbekmamytbekov545@gmail.com>
2023-01-20 19:41:32 +0600

    414. Third Maximum Number

--- 448. Find All Numbers Disappeared in an Array/index.ts
+++ 448. Find All Numbers Disappeared in an Array/index.ts
@@ -20,7 +20,7 @@ function findDisappearedNumbers(nums: number[]): number[] {
     const set = new Set(nums) , result = []
     for (let i = 1; i < nums.length+1; i++) { 
         if (!set.has(i)){
-            result.push(i)
+            result.push(i as never)
         }
     }
     return result
