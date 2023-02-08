05a53d65bc1de8171b310636ec5328da3800fa2a
Omurbek <omurbekmamytbekov545@gmail.com>
2023-02-08 17:28:25 +0600

    392. Is Subsequence

--- 392. Is Subsequence/index.ts
+++ 392. Is Subsequence/index.ts
@@ -1,3 +1,24 @@
+/**
+ * 
+ * @param s 
+ * @param t 
+ * @returns 
+ * Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+ 
+
+Example 1:
+
+Input: s = "abc", t = "ahbgdc"
+Output: true
+Example 2:
+
+Input: s = "axc", t = "ahbgdc"
+Output: false
+ */
+
 function isSubsequence(s: string, t: string): boolean {
     let p1 = 0,
         p2 = 0
@@ -9,6 +30,6 @@ function isSubsequence(s: string, t: string): boolean {
             p2++
         }
     }
-    return p1===s.length
+    return p1 === s.length
 
 };
