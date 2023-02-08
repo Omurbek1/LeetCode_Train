a32dde0d10da66ed189594d13135c6b9aa0386ac
Omurbek <omurbekmamytbekov545@gmail.com>
2023-02-08 17:27:52 +0600

    392. Is Subsequence

--- 392. Is Subsequence/index.ts
+++ 392. Is Subsequence/index.ts
@@ -1,6 +1,7 @@
 function isSubsequence(s: string, t: string): boolean {
-    let p1 = 0, p2 = 0
-    while (p1 < s.length && p2 > t.length) {
+    let p1 = 0,
+        p2 = 0
+    while (p1 < s.length && p2 < t.length) {
         if (s[p1] === t[p2]) {
             p1++
             p2++
