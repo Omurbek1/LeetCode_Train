b7f5743387afaba90608a2fbc19b36d5b2880756
Omurbek1 <omurbekmamytbekov545@gmail.com>
2023-02-11 00:34:08 +0600

    263 . Уродливый номер

--- 263. Ugly Number/index.ts
+++ 263. Ugly Number/index.ts
@@ -1,3 +1,23 @@
-function isUgly(n: number): boolean {
+const isUgly=(n: number): boolean=> {
+    if(n===0) return false;
+    while(n!=1){
+        if(n%2===0){
+            n=n/2;
+            continue;
+        }
+        if(n%3===0){
+            n=n/3
+            continue
+        }
+        if(n%5===0){
+            n=n/5
+            continue
+        }
+        return false
+    }
+    return true;
 
 };
+console.log(isUgly(6))
+console.log(isUgly(14))
+console.log(isUgly(5))
