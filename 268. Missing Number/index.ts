1ea2db7dd139077c59d6940fe6f3ef4132d46873
Omurbek1 <omurbekmamytbekov545@gmail.com>
2023-02-11 00:15:46 +0600

    268 Missing NUmber

---
+++ 268. Missing Number/index.ts
@@ -0,0 +1,53 @@
+/**
+ * 
+ * @param nums 
+ * @returns 
+ * 268. Missing Number
+Easy
+8.6K
+3K
+Companies
+Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+ 
+
+Example 1:
+
+Input: nums = [3,0,1]
+Output: 2
+Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+Example 2:
+
+Input: nums = [0,1]
+Output: 2
+Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+Example 3:
+
+Input: nums = [9,6,4,2,3,5,7,0,1]
+Output: 8
+Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
+ 
+
+Constraints:
+rm
+n == nums.length
+1 <= n <= 104
+0 <= nums[i] <= n
+All the numbers of nums are unique.
+ */
+const missingNumber=(nums: number[]): number =>{
+    let missingNumberSum=0;
+    let correctTotal=0;
+
+    for(let i=0;i<(nums.length+1);i++){
+        console.log(i)
+        correctTotal+=i;
+
+        if(nums[i]){
+            console.log(i)
+            missingNumberSum+=nums[i]
+        }
+    }
+
+    return (correctTotal-missingNumberSum)
+};
