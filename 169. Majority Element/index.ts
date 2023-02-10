9cfb7e6646895761ff60b2567d4716f6754dddec
Omurbek1 <omurbekmamytbekov545@gmail.com>
2023-02-11 00:24:48 +0600

    169. Majority Element

--- 169. Majority Element/index.ts
+++ 169. Majority Element/index.ts
@@ -1,3 +1,27 @@
+/**
+ * 
+ * @param nums 
+ * @returns 
+ * 169. Majority Element
+Easy
+13.6K
+428
+Companies
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+Example 2:
+
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+ */
 const majorityElement=(nums: number[]): number=> {
     let obj={};
     let count=0;
