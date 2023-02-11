40717cac81daebf62be6484977d7498d90db0b03
Omurbek1 <omurbekmamytbekov545@gmail.com>
2023-02-11 19:16:21 +0600

    383. Ransom Note

--- 263. Ugly Number/index.ts
+++ 263. Ugly Number/index.ts
@@ -1,5 +1,8 @@
 const isUgly=(n: number): boolean=> {
+    //TODO: Бул жерден кылдык
     if(n===0) return false;
+    //FIXME
+    //FIXME 
     while(n!=1){
         if(n%2===0){
             n=n/2;
