be19d3308f31d88fea6c01e216502dbc0eee6c70
Omurbek1 <omurbekmamytbekov545@gmail.com>
2023-02-13 11:06:39 +0600

    283. Move Zeroes

--- 283. Move Zeroes/index.ts
+++ 283. Move Zeroes/index.ts
@@ -1,6 +1,31 @@
 /**
  Do not return anything, modify nums in-place instead.
  */
+
+ /**
+  * 
+  * @param nums 
+  * @returns 
+  * 283. Move Zeroes
+Easy
+12.7K
+322
+Companies
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+ 
+
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+  */
 function moveZeroes(nums: number[]): void {
     let index=0;
     for(let i=0;i<nums.length;i++){
