cfaa204c2a1fa298a54af60ac728374b389ed344
Omurbek1 <omurbekmamytbekov545@gmail.com>
2023-02-14 13:16:02 +0600

    434. Number of Segments in a String

--- 434. Number of Segments in a String/index.ts
+++ 434. Number of Segments in a String/index.ts
@@ -1,3 +1,29 @@
+/**
+ * 
+ * @param s 
+ * @returns 
+ * 434. Number of Segments in a String
+Easy
+611
+1.1K
+Companies
+Given a string s, return the number of segments in the string.
+
+A segment is defined to be a contiguous sequence of non-space characters.
+
+ 
+
+Example 1:
+
+Input: s = "Hello, my name is John"
+Output: 5
+Explanation: The five segments are ["Hello,", "my", "name", "is", "John"]
+Example 2:
+
+Input: s = "Hello"
+Output: 1
+ */
+
 function countSegments(s: string): number {
 
     let segment = 0;
