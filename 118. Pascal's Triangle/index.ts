6d23bc6e086225d507eb002bb38e4e78ab94b9e7
Omurbek1 <omurbekmamytbekov545@gmail.com>
2023-02-14 13:51:45 +0600

    118. Pascal's Triangle

--- 118. Pascal's Triangle/index.ts
+++ 118. Pascal's Triangle/index.ts
@@ -27,12 +27,12 @@ Output: [[1]]
 const generate = (numRows: number): number[][] => {
     const table = [];
     for (let i = 0; i < numRows; i++) {
-        table[i] = [];
-        table[i][0] = 1;
+        table[i] =  [] as never;
+        table[i][0] = 1 as never;
         for (let j = 1; j < i; j++) {
-            table[i][j] = table[i - 1][j - 1] + table[i - 1][j]
+            table[i][j] = table[i - 1][j - 1] + table[i - 1][j] as never
         }
-        table[i][i] = 1;
+        table[i][i] = 1 as never;;
     }
     return table;
 
