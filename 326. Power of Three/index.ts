60f60b053e754258a98f46833d63bb832e6a4c27
Omurbek1 <omurbekmamytbekov545@gmail.com>
2023-02-15 16:41:48 +0600

    326. Power of Three

--- 326. Power of Three/index.ts
+++ 326. Power of Three/index.ts
@@ -1,3 +1,36 @@
+/**
+ * 
+ * @param n 
+ * @returns 
+ * 326. Power of Three
+Easy
+2.4K
+234
+Companies
+Given an integer n, return true if it is a power of three. Otherwise, return false.
+
+An integer n is a power of three, if there exists an integer x such that n == 3x.
+
+ 
+
+Example 1:
+
+Input: n = 27
+Output: true
+Explanation: 27 = 33
+Example 2:
+
+Input: n = 0
+Output: false
+Explanation: There is no x where 3x = 0.
+Example 3:
+
+Input: n = -1
+Output: false
+Explanation: There is no x where 3x = (-1).
+ */
+
+
 function isPowerOfThree(n: number): boolean {
     if(n===0) return false;
     while(n%3==0){
