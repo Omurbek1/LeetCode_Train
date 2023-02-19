7d606a52e1863cce97a93e8b4b688cdaae3b40a4
Omurbek1 <omurbekmamytbekov545@gmail.com>
2023-02-19 12:03:10 +0600

    103. Binary Tree Zigzag Level Order Traversal

--- 103. Binary Tree Zigzag Level Order Traversal/index.ts
+++ 103. Binary Tree Zigzag Level Order Traversal/index.ts
@@ -10,6 +10,29 @@
  *         this.right = (right===undefined ? null : right)
  *     }
  * }
+ * 
+ * 103. Binary Tree Zigzag Level Order Traversal
+Medium
+8.3K
+220
+Companies
+Given the root of a binary tree, return the zigzag level order traversal of its nodes' values. (i.e., from left to right, then right to left for the next level and alternate between).
+
+ 
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: [[3],[20,9],[15,7]]
+Example 2:
+
+Input: root = [1]
+Output: [[1]]
+Example 3:
+
+Input: root = []
+Output: []
  */
 
 function zigzagLevelOrder(root: TreeNode | null): number[][] {
