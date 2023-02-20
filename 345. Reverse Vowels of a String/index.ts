4971113c6d220a0766849ce56e3d13427d6b87ad
Omurbek1 <omurbekmamytbekov545@gmail.com>
2023-02-20 13:05:54 +0600

    345. Reverse Vowels of a String

--- 345. Reverse Vowels of a String/index.ts
+++ 345. Reverse Vowels of a String/index.ts
@@ -1,33 +1,33 @@
 function reverseVowels(s: string): string {
-    const vowels={}
-    for(const char of 'aeiouAEIOU'){
-vowels[char]=true;
+    const vowels = {}
+    for (const char of 'aeiouAEIOU') {
+        vowels[char] = true;
     }
 
-    const characters=s.split('');
+    const characters = s.split('');
 
-    let left=0;
-    let right=s.length-1;
+    let left = 0;
+    let right = s.length - 1;
 
-    while(left<right){
-        while(left<right && !(s[left] in vowels)){
+    while (left < right) {
+        while (left < right && !(s[left] in vowels)) {
             left++;
         }
-        
-        while(left<right && !(s[right] in vowels)){
+
+        while (left < right && !(s[right] in vowels)) {
             right--;
         }
-swap(characters,left,right);
-left++;
-right--;
-        
+        swap(characters, left, right);
+        left++;
+        right--;
+
     }
     return characters.join('');
 };
 
-const swap=(arr,i,j)=>{
-    const temp=arr[i];
-    arr[i]=arr[j];
-    arr[j]=temp;
+const swap = (arr, i, j) => {
+    const temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
 
 }
