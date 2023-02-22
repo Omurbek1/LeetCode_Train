1cea626d8ef6c2cf77da18a168ae002520531a01
Omurbek1 <omurbekmamytbekov545@gmail.com>
2023-02-22 22:51:04 +0600

    520. Detect Capital

---
+++ 520. Detect Capital/index.ts
@@ -0,0 +1,17 @@
+const detectCapitalUse=(word: string): boolean =>{
+    let capitalCount=0;
+
+    for(let i=0;i<word.length;i++){
+        const char=word[i];
+        if(char.toUpperCase()){
+            capitalCount++
+        }
+    }
+
+    return(
+        capitalCount===word.length||
+        capitalCount===0||
+       ( word[0]===word[0].toUpperCase() && capitalCount===1)
+       )
+
+};
