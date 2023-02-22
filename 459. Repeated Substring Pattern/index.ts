d9c2454efd2cfc9b8ed224ee8aca08abbdb80963
Omurbek1 <omurbekmamytbekov545@gmail.com>
2023-02-22 22:38:29 +0600

    459. Repeated Substring Pattern

--- 459. Repeated Substring Pattern/index.ts
+++ 459. Repeated Substring Pattern/index.ts
@@ -1,1 +1,26 @@
-const repeatedSubstringPattern=(s)=>s.repeat(2).slice(1,-1).includes(s)
+/**
+ * 
+ * @param s 
+ * @returns 
+ * Given a string s, check if it can be constructed by taking a substring of it and appending multiple copies of the substring together.
+
+ 
+
+Example 1:
+
+Input: s = "abab"
+Output: true
+Explanation: It is the substring "ab" twice.
+Example 2:
+
+Input: s = "aba"
+Output: false
+Example 3:
+
+Input: s = "abcabcabcabc"
+Output: true
+Explanation: It is the substring "abc" four times or the substring "abcabc" twice.
+ */
+
+// 1.функция принимает строку
+const repeatedSubstringPattern = (s) => s.repeat(2).slice(1, -1).includes(s)
