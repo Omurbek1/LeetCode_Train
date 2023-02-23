17735fae2f3c561f5aa3765a52f952091705bbc2
Omurbek1 <omurbekmamytbekov545@gmail.com>
2023-02-23 16:11:40 +0600

    605. Can Place Flowers

--- 605. Can Place Flowers/index.ts
+++ 605. Can Place Flowers/index.ts
@@ -1,4 +1,4 @@
-function canPlaceFlowers(flowerbed: number[], n: number): boolean {
+const canPlaceFlowers=(flowerbed: number[], n: number): boolean =>{
     if (n === 0) return true;
     for (let i = 0; i < flowerbed.length; i++) {
         if (flowerbed[i] !== 1 && flowerbed[i - 1] !== 1 &&
