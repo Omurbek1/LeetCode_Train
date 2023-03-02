c856a74b041e9b97de4f9ee464b8419abc03ccdf
Omurbek1 <omurbekmamytbekov545@gmail.com>
2023-03-02 09:50:01 +0600

    482. License Key Formatting

---
+++ 482. License Key Formatting/index.ts
@@ -0,0 +1,11 @@
+function licenseKeyFormatting(s: string, k: number): string {
+    let newStr = s.replace(/-/g, '').toUpperCase()
+    let arr = newStr.split('')
+
+    for (let i = newStr.length - 1 - k; i >= 0; i -= k) {
+        arr[i] = arr[i] + '-'
+    }
+
+    return arr.join('')
+    // return result.split('').reverse().join('')
+};
