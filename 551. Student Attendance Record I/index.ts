8755c7d01a3b7709d1ea7170a576f2c512719371
Omurbek1 <omurbekmamytbekov545@gmail.com>
2023-03-02 13:05:21 +0600

    551. Student Attendance Record I

---
+++ 551. Student Attendance Record I/index.ts
@@ -0,0 +1,17 @@
+function checkRecord(s: string): boolean {
+    let absent=0
+    let late=0
+    for(let i=0;i<s.length;i++){
+        if(s[i]==='A'){
+            absent++
+           
+        }if(absent>=2) return false
+        if(s[i]==='L'){
+            late++
+        }else{
+            late=0
+        }
+        if(late>=3) return false
+    }
+    return true
+};
