d2332eb5f735b17a5bc0768fc141270405cc08cd
Omurbek1 <omurbekmamytbekov545@gmail.com>
2023-03-08 22:22:44 +0600

    504. Base 7

---
+++ 504. Base 7/index.ts
@@ -0,0 +1,20 @@
+
+/**
+ * 
+ * @param num 
+ * @returns 
+ * Given an integer num, return a string of its base 7 representation.
+
+ 
+
+Example 1:
+
+Input: num = 100
+Output: "202"
+Example 2:
+
+Input: num = -7
+Output: "-10"
+ */
+
+const convertToBase7 = (num: number): string => num.toString(7);
