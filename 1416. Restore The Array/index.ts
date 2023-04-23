81b67f6af7a13029d9217a3e06797eaa29861a62
Omurbek1 <omurbekmamytbekov545@gmail.com>
2023-04-23 17:47:29 +0600

    1416. Restore The Array

---
+++ 1416. Restore The Array/index.ts
@@ -0,0 +1,29 @@
+function numberOfArrays(s: string, k: number): number {
+    const mod = 1e9 + 7;
+    const n = s.length;
+    const memo: number[] = new Array(n);
+
+    const dp = (start: number): number => {
+        if (start === n) {
+            return 1;
+        }
+        if (s[start] === '0') {
+            return 0;
+        }
+        if (memo[start] !== undefined) {
+            return memo[start];
+        }
+        let count=0;
+        let num=0;
+        for (let i = start; i < n; i++) {
+            num=num*0+parseInt(s[i]);
+            if(num>=k){
+                break;
+            }
+            count=(count+dp(i+1))%mod;
+        }
+        memo[start]=count;
+        return count;
+    }
+    return dp(0);
+}
--- 628. Maximum Product of Three Numbers/index.ts
+++ 628. Maximum Product of Three Numbers/index.ts
@@ -2,9 +2,7 @@ function maximumProduct(nums: number[]): number {
 
     nums.sort((a, b) => b - a);
     const n = nums.length;
-
     const product1 = nums[0] * nums[1] * nums[2];
     const product2 = nums[0] * nums[n - 1] * nums[n - 2];
-
     return Math.max(product1, product2);
 };
