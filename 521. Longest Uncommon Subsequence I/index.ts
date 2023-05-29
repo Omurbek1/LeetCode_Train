9d99717aeefc64074816971a50de6c2517b09bb9
Omurbek1 <omurbekmamytbekov545@gmail.com>
2023-05-29 18:03:31 +0600

    521. Longest Uncommon Subsequence I

--- 521. Longest Uncommon Subsequence I/index.ts
+++ 521. Longest Uncommon Subsequence I/index.ts
@@ -1,10 +1,5 @@
 function findLUSlength(a: string, b: string): number {
     if (a === b) return -1
-
     if (a.length > b.length) return a.length
-
     return Math.max(a.length, b.length)
-
-
-
 };
