53adc1df1fca0494eb9fadc63ad6d0dec037bfce
Omurbek1 <omurbekmamytbekov545@gmail.com>
2023-06-02 10:01:27 +0600

    13. Roman to Integer

--- 169. Majority Element/index.ts
+++ 169. Majority Element/index.ts
@@ -22,23 +22,23 @@ Example 2:
 Input: nums = [2,2,1,1,1,2,2]
 Output: 2
  */
-const majorityElement=(nums: number[]): number=> {
-    let obj={};
-    let count=0;
-    let max=0;
+const majorityElement = (nums: number[]): number => {
+    let obj = {};
+    let count = 0;
+    let max = 0;
 
-    for(let i of nums){
-        if(!obj[i]){
-            obj[i]=1
-        }else{
+    for (let i of nums) {
+        if (!obj[i]) {
+            obj[i] = 1
+        } else {
             obj[i]++
         }
     }
-    for(let j in obj){
-        if(obj[j]>count){
-count=obj[j];
-max=j as never;
+    for (let j in obj) {
+        if (obj[j] > count) {
+            count = obj[j];
+            max = j as never;
         }
     }
-return max
+    return max
 };
