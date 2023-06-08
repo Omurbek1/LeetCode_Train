e5b0480fa4dd53217cdc625d71636b9d2db73c7f
Omurbek1 <omurbekmamytbekov545@gmail.com>
2023-06-08 23:20:00 +0600

    14. Longest Common Prefix

--- 14. Longest Common Prefix/index.ts
+++ 14. Longest Common Prefix/index.ts
@@ -10,7 +10,7 @@ function longestCommonPrefix(strs: string[]): string {
      let prefix = strs[0];
 
      for(let i=0;i<strs.length;i++){
-        while(strs[i] && strs[i].indexOf(prefix)!==0){
+        while( strs[i].indexOf(prefix)!==0){
             prefix=prefix.substring(0,prefix.length-1);
         }
      }
