d8fa0408e66782cf9382a27f54b2d8407b9049c5
Omurbek1 <omurbekmamytbekov545@gmail.com>
2023-06-12 11:39:48 +0600

    55. Jump Game

---
+++ 55. Jump Game/index.ts
@@ -0,0 +1,19 @@
+/**
+ * 
+ * @param nums 
+ * @returns 
+ * 
+ * You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+Return true if you can reach the last index, or false otherwise.
+ */
+
+function canJump(nums: number[]): boolean {
+    let max = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (i > max) return false;
+        max = Math.max(max, i + nums[i])
+    }
+    return true;
+
+};
