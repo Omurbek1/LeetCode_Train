cc367b11f2f1f61d901451a5e56f6bcee8ed73f1
Omur <omurbekmamytbekov545@gmail.com>
2023-10-04 10:28:51 +0600

    41. First Missing Positive

--- 41. First Missing Positive/index.ts
+++ 41. First Missing Positive/index.ts
@@ -1,18 +1,6 @@
 function firstMissingPositive(nums: number[]): number {
-    let i = 0;
-    while (i < nums.length) {
-        let correctIndex = nums[i] - 1;
-        if(nums[i] !== nums[correctIndex]) {
-            [nums[i], nums[correctIndex]] = [nums[correctIndex], nums[i]];
-        } else {
-            i++;
-        }
-    }
-
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] !== i + 1) {
-            return i + 1;
-        }
-    }
-return nums.length + 1
+    const set=new Set(nums)
+    let i=1
+    while(set.has(i))i++
+    return i
 };
