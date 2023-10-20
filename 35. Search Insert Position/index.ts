7937af3169f78351adf95a1e7281e71c30eb9be6
Omur <omurbekmamytbekov545@gmail.com>
2023-10-20 11:08:09 +0600

    35. Поиск позиции вставки

---
+++ 35. Search Insert Position/index.ts
@@ -0,0 +1,16 @@
+function searchInsert(nums: number[], target: number): number {
+let left: number =0
+let right: number =nums.length-1
+while(left<=right){
+    let mid=left+Math.floor((right-left)/2)
+    if(nums[mid]===target){
+        return mid
+    }else if(nums[mid]<target){
+        left=mid+1
+    }else{
+        right=mid-1
+    }
+}
+return left
+
+};
