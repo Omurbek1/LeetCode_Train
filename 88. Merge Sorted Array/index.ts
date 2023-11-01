47124d9192ab73a563c3d470e3eecd4e7f87b205
Omur <omurbekmamytbekov545@gmail.com>
2023-11-01 22:50:41 +0600

    88. Merge Sorted Array

--- 88. Merge Sorted Array/index.ts
+++ 88. Merge Sorted Array/index.ts
@@ -2,31 +2,25 @@
  Do not return anything, modify nums1 in-place instead.
  */
 function merge(nums1: number[], m: number, nums2: number[], n: number): void {
-
-    let i=0, k=0, j=0;
-
-    while(i<m && j<n){
-        if(nums1[i]<nums2[j]){
-            i++;
-
-        }else{
-            nums1[k++] = nums2[j++];
+    let i = m - 1;
+    let j = n - 1;
+    let k = m + n - 1;
+    
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+    
+            k--;
         }
-
-    }
-    while(i<m){
-        nums1[k++] = nums1[i++];
-
-    }
-
-    while(j<n){
-        nums1[k++] = nums2[j++];
-
-    }
-
-
     
-
-    
-
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
 };
