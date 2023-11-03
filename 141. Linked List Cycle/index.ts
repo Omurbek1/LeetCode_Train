205ce7931e0278262ae5424b78b5b551f765dd0d
Omur <omurbekmamytbekov545@gmail.com>
2023-11-03 11:10:55 +0600

    141. Linked List Cycle

--- 141. Linked List Cycle/index.ts
+++ 141. Linked List Cycle/index.ts
@@ -10,19 +10,14 @@
  * }
  */
  function hasCycle(head: ListNode | null): boolean {
-    if (!head) return false;
-
-    let slow = head;
-    let fast = head;
-    while (fast) {
-        if (!fast.next) {
-            return false
+    let slow = head
+    let fast = head
+    while (fast && fast.next) {
+        slow = slow.next
+        fast = fast.next.next
+        if (slow === fast) {
+            return true
         }
-        else {
-            fast = fast.next.next
-            slow = slow.next
-        }
-        if (fast === slow) return true
     }
     return false
 
