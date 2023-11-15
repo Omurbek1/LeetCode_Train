d0931bde5e913182b9aa1ef9c744df5aec6477cc
Omur <omurbekmamytbekov545@gmail.com>
2023-11-15 09:27:37 +0600

    112. Path Sum

--- 112. Path Sum/index.ts
+++ 112. Path Sum/index.ts
@@ -1,6 +1,20 @@
-var hasPathSum = function(root, sum) {
-    if(!root) return false
-    sum -= root.val
-    if(!root.right && !root.left) return !sum
-    return hasPathSum(root.left, sum) || hasPathSum(root.right, sum)
-}
+// class TreeNode {
+//     val: number
+//     left: TreeNode | null
+//     right: TreeNode | null
+//     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+//         this.val = (val===undefined ? 0 : val)
+//         this.left = (left===undefined ? null : left)
+//         this.right = (right===undefined ? null : right)
+//     }
+// }
+
+
+
+function hasPathSum(root: TreeNode | null, targetSum: number): boolean {
+    if (!root) return false
+    targetSum -= root.val
+    if (!root.right && !root.left) return !targetSum
+    return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum)
+
+};
