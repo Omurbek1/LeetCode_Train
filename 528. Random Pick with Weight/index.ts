0f5b943f5796929b6de225a1e345c6ad2c8c9a4e
Omur <omurbekmamytbekov545@gmail.com>
2023-11-22 12:42:14 +0600

    528. Random Pick with Weight

---
+++ 528. Random Pick with Weight/index.ts
@@ -0,0 +1,34 @@
+class Solution {
+    weights: number[]
+
+    constructor(w: number[]) {
+        const sum = w.reduce((a, b) => a + b)
+        this.weights = w
+        for (let i = 0; i < w.length; i++) {
+            const num = w[i]
+            this.weights[i] = num / sum
+            if (i > 0) this.weights[i] += this.weights[i - 1]
+        }
+
+
+    }
+
+    pickIndex(): number {
+        const randomNum = Math.random()
+        let left = 0
+        let right = this.weights.length - 1
+        while (left < right) {
+            const mid = Math.floor((left + right) / 2)
+            if (randomNum > this.weights[mid]) left = mid + 1
+            else right = mid
+
+        }
+        return left
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * var obj = new Solution(w)
+ * var param_1 = obj.pickIndex()
+ */
