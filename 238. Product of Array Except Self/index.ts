92227e2488a7f791bdbf3f3a32523aecdbfd5d44
Omur <omurbekmamytbekov545@gmail.com>
2023-12-07 10:55:11 +0600

    238. Product of Array Except Self

---
+++ 238. Product of Array Except Self/index.ts
@@ -0,0 +1,14 @@
+function productExceptSelf(nums: number[]): number[] {
+    const res = new Array(nums.length).fill(1);
+    let left = 1;
+    for (let i = 0; i < nums.length; i++) {
+        res[i] = left;
+        left *= nums[i];
+    }
+    let right = 1;
+    for (let i = nums.length - 1; i >= 0; i--) {
+        res[i] *= right;
+        right *= nums[i];
+    }
+    return res;
+};
