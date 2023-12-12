3620ba49d3cf01cfd3414e6d20d65778f3aca326
Omur <omurbekmamytbekov545@gmail.com>
2023-12-12 19:48:21 +0600

    4. Median of Two Sorted Arrays

--- 4. Median of Two Sorted Arrays/index.ts
+++ 4. Median of Two Sorted Arrays/index.ts
@@ -1,38 +1,30 @@
-var findMedianSortedArrays = function(nums1, nums2) {
-	let len1:number = nums1.length;
-	let len2:number = nums2.length;
-	let total:any = len1 + len2;
-	if (total % 2) {
-		return findKthOfTwoSortedArrays(nums1, len1, nums2, len2, Number.parseInt(total / 2 + 1));
-	} else {
-		return (
-			(findKthOfTwoSortedArrays(nums1, len1, nums2, len2, total / 2) +
-				findKthOfTwoSortedArrays(nums1, len1, nums2, len2, total / 2 + 1)) /
-			2
-		);
+function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
+	let nums: number[] = [];
+
+	const merge = (arr1: number[], arr2: number[]): number[] => {
+		while (arr1.length && arr2.length) {
+			if (arr1[0] < arr2[0]) {
+				nums.push(arr1.shift());
+			} else {
+				nums.push(arr2.shift());
+			}
+		}
+
+		nums = [...nums, ...arr1, ...arr2];
+		return nums;
+	}
+
+	merge(nums1, nums2);
+	if (nums.length % 2 == 1) return nums[Math.floor(nums.length / 2)];
+	else {
+		let left: number = 0;
+		let right: number = nums.length - 1;
+
+		while (left < right) {
+			left++;
+			right--;
+		}
+
+		return (nums[left] + nums[right]) / 2;
 	}
 };
-function findKthOfTwoSortedArrays(p, m, q, n, k) {
-	if (m > n) {
-		return findKthOfTwoSortedArrays(q, n, p, m, k);
-	}
-
-	if (m === 0) {
-		return q[k - 1];
-	}
-
-	if (k === 1) {
-		return Math.min(p[0], q[0]);
-	}
-
-	var pa = Math.min(parseInt(k / 2), m);
-	var qa = k - pa;
-
-	if (p[pa - 1] < q[qa - 1]) {
-		return findKthOfTwoSortedArrays(p.slice(pa), m - pa, q, n, k - pa);
-	} else if (q[qa - 1] < p[pa - 1]) {
-		return findKthOfTwoSortedArrays(p, m, q.slice(qa), n - qa, k - qa);
-	} else {
-		return p[pa - 1];
-	}
-}
