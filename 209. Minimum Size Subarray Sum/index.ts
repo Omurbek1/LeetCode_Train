2223626f4e23ee0e97a68c4e576ddc5cf18136a1
Omur <omurbekmamytbekov545@gmail.com>
2023-12-15 18:12:25 +0600

    209. Minimum Size Subarray Sum

--- 209. Minimum Size Subarray Sum/index.ts
+++ 209. Minimum Size Subarray Sum/index.ts
@@ -1,24 +1,21 @@
-/**
- * @param {number} target
- * @param {number[]} nums
- * @return {number}
- */
-var minSubArrayLen = function (searchNumber, arr) {
-    let result = Number.MAX_SAFE_INTEGER
-    let left = 0
-    let sum = 0
-    
-    // for loop
+function minSubArrayLen(target: number, nums: number[]): number {
+    let left = 0; // Left pointer to track the start of the current subarray
+    let right = 0; // Right pointer to expand the current subarray
 
-    for (let i = 0; i < arr.length; i++){
-        sum += arr[i]
-        
-        while (sum >= searchNumber) {
-            result = Math.min(result, i - left + 1)
-            sum-=arr[left++]
+    let sum = 0; // Sum of elements in the current subarray
+    let minLen = Number.MAX_SAFE_INTEGER; // Minimum length of a subarray with a sum >= target
+    const n = nums.length; // Length of the input array
+
+    while (right < n) {
+        sum += nums[right]; // Expand the current subarray by including the right element
+        while (sum >= target) {
+            // If the sum of the current subarray is greater than or equal to the target
+            minLen = Math.min(minLen, right - left + 1); // Update the minimum length
+            sum -= nums[left]; // Contract the subarray from the left by excluding the left element
+            left++; // Move the left pointer to the right
         }
+        right++; // Move the right pointer to the right to expand the subarray
     }
-    return result===Number.MAX_SAFE_INTEGER ? 0 :result
-};
 
-const arr=[1,1,2,3,]
+    return minLen === Number.MAX_SAFE_INTEGER ? 0 : minLen; // Return the minimum length
+}
