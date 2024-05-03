b8c202233ef00322044f9673e6a587f46a68c8a8
Omur <omurbekmamytbekov545@gmail.com>
2024-05-03 14:54:27 +0600

    165. Compare Version Numbers

--- 165. Compare Version Numbers/index.ts
+++ 165. Compare Version Numbers/index.ts
@@ -6,25 +6,11 @@ function compareVersion(version1: string, version2: string): number {
 
   while (i < v1.length || i < v2.length) {
     const num1 = i < v1.length ? parseInt(v1[i]) : 0;
-    const num2 = i<v2.length ? parseInt(v2[i]) : 0;
+    const num2 = i < v2.length ? parseInt(v2[i]) : 0;
 
     if (num1 < num2) return -1;
     if (num1 > num2) return 1;
     i++;
   }
   return 0;
-  // const v1 = version1.split('.');
-  // const v2 = version2.split('.');
-
-  // let i = 0;
-  // while (i < v1.length || i < v2.length) {
-  //     const num1 = i < v1.length ? parseInt(v1[i]) : 0;
-  //     const num2 = i < v2.length ? parseInt(v2[i]) : 0;
-
-  //     if (num1 < num2) return -1;
-  //     if (num1 > num2) return 1;
-  //     i++;
-  // }
-
-  // return 0;
 }
